154113495cdfbc2a10d68de034196016402e9a40
VitoInfinito <tomas@hasselquist.se>
2015-12-14 15:41:44 +0100

    Structured code, removed unused code and fixed last bugs

--- lab2/pintos/src/devices/batch-scheduler.c
+++ lab2/pintos/src/devices/batch-scheduler.c
@@ -37,19 +37,9 @@ void oneTask(task_t task);/*Task requires to use the bus and executes methods be
 	void transferData(task_t task); /* task processes data on the bus either sending or receiving based on the direction*/
 	void leaveSlot(task_t task); /* task release the slot */
 
-struct semaphore channel, mutex;
-struct semaphore sender;
-struct semaphore sender_prio;
-struct semaphore receiver;
-struct semaphore receiver_prio;
+struct semaphore channel, mutex, sender, sender_prio, receiver, receiver_prio;
 
-static int direction;
-static int wait_for_channel;
-static int in_channel;
-static int wait_send;
-static int wait_rec;
-static int wait_high_send;
-static int wait_high_rec;
+static int direction, wait_for_channel, in_channel, wait_send, wait_rec, wait_high_send, wait_high_rec, debug;
 
 
 /* initializes semaphores */ 
@@ -58,19 +48,14 @@ void init_bus(void){
     random_init((unsigned int)123456789); 
 
     sema_init(&mutex, 1);
-    sema_init(&channel, 3);
     sema_init(&sender, 0);
     sema_init(&receiver, 0);
     sema_init(&sender_prio, 0);
     sema_init(&receiver_prio, 0);
 
     direction = -1;
-    in_channel = 0;
-    wait_for_channel = 0;
-    wait_send = 0;
-    wait_rec = 0;
-    wait_high_send = 0;
-    wait_high_rec = 0;
+    in_channel = wait_send = wait_rec = wait_high_send = wait_high_rec = 0;
+    debug = 1;
 }
 
 /*
@@ -89,7 +74,8 @@ void batchScheduler(unsigned int num_tasks_send, unsigned int num_task_receive,
 {
     unsigned int i;
 
-    printf("New batch with send: %d, rec: %d, hsend: %d, hrec: %d\n", num_tasks_send, num_task_receive, num_priority_send, num_priority_receive);
+    if (debug)
+        printf("New batch with send: %d, rec: %d, hsend: %d, hrec: %d\n", num_tasks_send, num_task_receive, num_priority_send, num_priority_receive);
 
     for (i=0; i<num_priority_send; i++) {
         thread_create ("send_prio", PRI_MAX, senderPriorityTask, 0);
@@ -148,16 +134,8 @@ void getSlot(task_t task)
         direction = task.direction;
     }
 
-    
-    /*&& ((task.priority == HIGH) ||(task.direction == SENDER && wait_high_rec == 0 ) || (task.direction == RECEIVER && wait_high_send == 0))*/
-    if (direction == task.direction && in_channel < 3 && ((task.priority == HIGH && ((task.direction == SENDER && wait_high_rec == 0) || (task.direction == RECEIVER && wait_high_send == 0))) || (wait_high_rec == 0 && wait_high_send == 0))) {
-        //Redundant. remove semaphore channel
-        //wait_for_channel++;
-        //sema_down(&channel);
-        //sema_down(&mutex);
-        //wait_for_channel--;
+    if (direction == task.direction && in_channel < 3 && ((task.priority == HIGH) || (wait_high_rec == 0 && wait_high_send == 0))) {
         in_channel++;
-        //sema_up(&mutex);
     } else {
         
         if (task.priority == HIGH) {
@@ -165,7 +143,6 @@ void getSlot(task_t task)
                 wait_high_send++;
                 sema_up(&mutex);
                 sema_down(&sender_prio);
-                printf("Sender prio was released\n");
                 sema_down(&mutex);
                 wait_high_send--;
             } else {
@@ -180,7 +157,6 @@ void getSlot(task_t task)
                 wait_send++;
                 sema_up(&mutex);
                 sema_down(&sender);
-                printf("Sender was released\n");
                 sema_down(&mutex);
                 wait_send--;
             } else {
@@ -192,10 +168,10 @@ void getSlot(task_t task)
             }
         }
 
-        //sema_down(&channel);
-        //sema_down(&mutex);
-        in_channel++;
-        //sema_up(&mutex);
+        //Recursively call getSlot to make sure we really can enter channel (Edge case)
+        sema_up(&mutex);
+        getSlot(task);
+        sema_down(&mutex);
     }
     sema_up(&mutex);
 }
@@ -203,52 +179,46 @@ void getSlot(task_t task)
 /* task processes data on the bus send/receive */
 void transferData(task_t task) 
 {
-    printf("S priority: %d, direction: %d + in_channel: %d\n", task.priority, task.direction, in_channel);
+    if (debug)
+        printf("S priority: %d, direction: %d + in_channel: %d\n", task.priority, task.direction, in_channel);
     //msg("Started transfer");
-    //Add some random sleep
 
     int64_t sleeptime = (int64_t)random_ulong();
     timer_sleep(sleeptime%100);
     //msg("Finished transfer");
-    printf("F priority: %d, direction: %d\n", task.priority, task.direction);
+    if (debug)
+        printf("F priority: %d, direction: %d\n", task.priority, task.direction);
 }
 
 /* task releases the slot */
 void leaveSlot(task_t task) 
 {
     sema_down(&mutex);
+    int i;
     in_channel--;
-    
 
-    //sema_up(&channel);
-
-    if (in_channel < 3) {
-        if ((direction == SENDER && (wait_high_send > 0 || (wait_high_rec == 0 && wait_send > 0))) || (direction == RECEIVER && in_channel == 0 && wait_high_rec == 0 && (wait_high_send > 0 || (wait_rec == 0 && wait_send > 0)))) {
-            direction = SENDER;
-            if (wait_high_send > 0) {
-                printf("release high send\n");
+    if ((direction == SENDER && (wait_high_send > 0 || (wait_high_rec == 0 && wait_send > 0))) || (direction == RECEIVER && in_channel == 0 && wait_high_rec == 0 && (wait_high_send > 0 || (wait_rec == 0 && wait_send > 0)))) {
+        direction = SENDER;
+        if (wait_high_send > 0) {
+            for (i=0; i<3-in_channel; i++) {
                 sema_up(&sender_prio);
-            } else {
-                printf("release low send\n");
+            }
+        } else {
+            for (i=0; i<3-in_channel; i++) {
                 sema_up(&sender);
             }
-            
-
-
-
-        } else if ((direction == RECEIVER && (wait_high_rec > 0 || (wait_high_send == 0 && wait_rec > 0))) || (direction == SENDER && in_channel == 0 && wait_high_send == 0 && (wait_high_rec > 0 || (wait_send == 0 && wait_rec > 0)))){
-            direction = RECEIVER;
-            if (wait_high_rec > 0) {
-                printf("release high rec\n");
+        }
+    } else if ((direction == RECEIVER && (wait_high_rec > 0 || (wait_high_send == 0 && wait_rec > 0))) || (direction == SENDER && in_channel == 0 && wait_high_send == 0 && (wait_high_rec > 0 || (wait_send == 0 && wait_rec > 0)))){
+        direction = RECEIVER;
+        if (wait_high_rec > 0) {
+            for (i=0; i<3-in_channel; i++) {
                 sema_up(&receiver_prio);
-            } else {
-                printf("release low rec\n");
+            }
+        } else {
+            for (i=0; i<3-in_channel; i++) {
                 sema_up(&receiver);
             }
-            
-        }
+        }   
     }
-    
     sema_up(&mutex);
-    
 }
